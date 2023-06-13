8772bdbe2fece5b40842a248edbdc8f34d7440dd
Nakazawa Haru <C0A2210876@edu.teu.ac.jp>
2023-06-13 17:26:27 +0900

    rensyuu6

--- pygame_image.py
+++ pygame_image.py
@@ -10,16 +10,43 @@ def main():
     tmr = 0
     kk_img = pg.image.load("ex01-20230613/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_imgs =[kk_img, pg.transform.rotozoom(kk_img,10,1.0)]
+    kk_img = pg.transform.rotozoom(kk_img,0,1.0)
+    kk_img2 = pg.transform.rotozoom(kk_img,1,1.0)
+    kk_img3 = pg.transform.rotozoom(kk_img,2,1.0)
+    kk_img4 = pg.transform.rotozoom(kk_img,3,1.0)
+    kk_img5 = pg.transform.rotozoom(kk_img,4,1.0)
+    kk_img6 = pg.transform.rotozoom(kk_img,5,1.0)
+    kk_img7 = pg.transform.rotozoom(kk_img,6,1.0)
+    kk_img8 = pg.transform.rotozoom(kk_img,7,1.0)
+    kk_img9 = pg.transform.rotozoom(kk_img,8,1.0)
+    kk_img10 = pg.transform.rotozoom(kk_img,9,1.0)
+    kk_img11 = pg.transform.rotozoom(kk_img,10,1.0)
+    kk_img12 = pg.transform.rotozoom(kk_img,9,1.0)
+    kk_img13 = pg.transform.rotozoom(kk_img,8,1.0)
+    kk_img14 = pg.transform.rotozoom(kk_img,7,1.0)
+    kk_img15 = pg.transform.rotozoom(kk_img,6,1.0)
+    kk_img16 = pg.transform.rotozoom(kk_img,5,1.0)
+    kk_img17 = pg.transform.rotozoom(kk_img,4,1.0)
+    kk_img18 = pg.transform.rotozoom(kk_img,3,1.0)
+    kk_img19 = pg.transform.rotozoom(kk_img,2,1.0)
+    kk_img20 = pg.transform.rotozoom(kk_img,1,1.0)
+    kk_imgs =[kk_img,kk_img2,kk_img3,kk_img4,kk_img5,kk_img6,kk_img7,kk_img8,
+              kk_img9,kk_img10,kk_img11,kk_img12,kk_img13,kk_img14,kk_img15,
+              kk_img16,kk_img17,kk_img18,kk_img19,kk_img20,1.0]
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_imgs[0],[300,200])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
+        screen.blit(kk_imgs[tmr%20],[300,200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        if tmr >=1600:
+            tmr = 0
+
+        clock.tick(100)
 
 
 if __name__ == "__main__":
